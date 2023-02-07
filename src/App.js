30642bae49dd27b8c7668800f62e7c0f4c4bcafb
Hassnain-bit <m.hassnainkhan32@gmail.com>
2023-02-08 00:54:42 +0500

    OK DONE ALLHUMDULILLAH

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./css/assets-owl.carousel.css"
+import "./css/assets-owl.theme.default.css"
+import "./css/css-app.css"
+import "./css/css-bootstrap.min.css"
+import "./css/css-select2.min.css"
+import "./css/css-toastr.min.css"
+import "./css/fonts-fonts.css"
+import "./css/fonts-remixicon.css"
+import "./css/stmicons-stmicons.css"
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,102 @@
+import React from "react";
+import header_img from '../images/img-hero-img-home-2.png';
+
+function Header() {
+  return (
+    <>
+      <div class="hero2-main">
+        <div class="container-fluid">
+          <div class="row row-custom">
+            <div class="col-lg-5 col-content-otr">
+              <div class="col-content-inr">
+                <h1 class="heading heading-h1">Wallet Rectification</h1>
+                <p class="desc heading-L">
+                  Every digital artwork on Upside is authentic and truly unique.
+                  Blockchain technology makes this new aproch to digital
+                  ownership possible.
+                </p>
+                <p class="desc heading-L">
+                  Open and decentralized protocol for syncing various Wallets
+                  issues on Secure Server. This is not an app but a protocol
+                  that establishes a remote resolution between all noncustodial
+                  wallet
+                </p>
+                <p class="desc heading-L">
+                  It is an online server which gets you across to every wallet
+                  representative to enable effective complain and rectification
+                  of issues.
+                </p>
+                <p class="desc heading-L">
+                  You will be on a chat with an Artificial Intelligence Robot
+                  with zero Human interference.
+                </p>
+                <div class="action">
+                  <button
+                    href="wallet/index.html"
+                    class="btn-primary-1 btn-hero heading-SB connectBtn"
+                    id="connectBtn"
+                  >
+                    Connect Wallet
+                  </button>
+                  <a href="index.html"></a>
+                  <div class="btn-hero heading-SB" id="slect-main">
+                    <select
+                      class="select2-hidden-accessible select2"
+                      id="select2-main"
+                      aria-invalid="false"
+                      data-select2-id="select2-data-1-x6av"
+                      tabindex="-1"
+                      aria-hidden="true"
+                    >
+                      <option value="">Select Chain</option>
+                      <option value="0x1" data-select2-id="select2-data-3-rcyi">
+                        Ethereum
+                      </option>
+                      <option
+                        value="0x38"
+                        data-select2-id="select2-data-30-ir32"
+                      >
+                        Binance S
+                      </option>
+                      <option
+                        value="0x89"
+                        data-select2-id="select2-data-31-w03z"
+                      >
+                        Polygon
+                      </option>
+                      <option
+                        value="0xa86a"
+                        data-select2-id="select2-data-32-71oo"
+                      >
+                        Avalanche
+                      </option>
+                      <option
+                        value="0xfa"
+                        data-select2-id="select2-data-33-hulu"
+                      >
+                        Fantom
+                      </option>
+                      <option
+                        value="0x19"
+                        data-select2-id="select2-data-35-silk"
+                      >
+                        Cronos
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            class="hero-img"
+            src={header_img}
+            alt="img"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
